114b8ae299fe9095c3e91205e7aa4a6bbfb3b842
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-07-25 22:30:36 +1000

    backend wiring up - profile and login

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,7 @@ class ProfilesController < ApplicationController
   # GET /profiles/new
   def new
     @profile = Profile.new
+    @user_type = params[:user_type] 
   end
 
   # GET /profiles/1/edit
@@ -25,6 +26,7 @@ class ProfilesController < ApplicationController
   # POST /profiles.json
   def create
     @profile = Profile.new(profile_params)
+    @profile.user_id = current_user.id
 
     respond_to do |format|
       if @profile.save
