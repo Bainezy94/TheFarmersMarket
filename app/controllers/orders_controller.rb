692c9758c6e673159379ca5459edbc2858fe4c0b
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-07-26 15:40:31 +1000

    drop downs in order form added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,7 +6,7 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
-
+ 
   # GET /orders/1
   # GET /orders/1.json
   def show
@@ -25,6 +25,7 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.profile_id = current_user.profile.id
 
     respond_to do |format|
       if @order.save
