26c0f876b1cfb7f48f89840d17c5e5c9cf58edc3
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-07-29 17:03:47 +1000

    working on dynamic form for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,6 +15,8 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    @product = Products.all
+    #need to add /kg or each etc
   end
 
   # GET /orders/1/edit
