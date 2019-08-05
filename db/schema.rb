c459b9567661129cd8ac933eaba318ff13ca82a4
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-08-05 13:00:13 +1000

    product added to seeds, image removed from product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_31_061155) do
+ActiveRecord::Schema.define(version: 2019_08_05_025806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,7 +78,6 @@ ActiveRecord::Schema.define(version: 2019_07_31_061155) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "image"
     t.text "description"
     t.string "price"
     t.date "picked_date"
