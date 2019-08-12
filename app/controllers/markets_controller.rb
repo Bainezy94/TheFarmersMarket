0797da1911d9f135616e6770ec1db9264915d566
David James Baines <d.baines@guihenjones.com.au>
2019-08-12 15:53:49 +1000

    Added CanCan

--- app/controllers/markets_controller.rb
+++ app/controllers/markets_controller.rb
@@ -1,5 +1,5 @@
 class MarketsController < ApplicationController
-  before_action :set_market, only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource
 
   # GET /markets
   # GET /markets.json
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+    load_and_authorize_resource
 
   # GET /reviews
   # GET /reviews.json
